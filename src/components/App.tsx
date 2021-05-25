42279c585bdfa4ddf7bc7fb441cbd80c1c7200b3
Mitchell <mitchell_shelton@y7mail.com>
2021-05-25 23:22:23 +1000

    Display quarter note and half note

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,12 +5,23 @@ import Sheet from "./Sheet";
 
 import '../styles/App.css';
 
+const notes = [
+  [
+    ['qn', 'qn', 'qn', 'qn'], ['qn', 'hn', 'qn'], ['hn', 'hn'], ['qn', 'qn', 'qn', 'qn']
+  ],
+  [
+    ['hn', 'hn'], ['hn', 'qn', 'qn'], ['qn', 'qn', 'hn'], ['qn', 'hn', 'qn']
+  ],
+  // [
+  //   ['qn', 'qn', 'qn', 'qn'], ['qn', 'hn', 'qn'], ['qn', 'qn', 'qn', 'qn'], ['hn', 'hn']
+  // ]
+]
 
 function App() {
   return (
     <Box minHeight="100vh" className="App">
       <p>Rhythm Expert</p>
-      <Sheet />
+      <Sheet notes={notes} />
     </Box>
   );
 }
--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -1,43 +1,22 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-
-
-import quarternote from '../img/notes/quarter-note-15x42.svg';
-import halfnote from '../img/notes/half-note-15x42.svg';
+import SheetRow from './SheetRow';
 
 export type SheetProps = {
-
+   notes: string[][][];
 }
 
-const Sheet = (props: SheetProps) => {
+const Sheet = ({notes}: SheetProps) => {
+
+    console.log("got notes", notes);
+
+    console.log("len", notes.length);
 
     return (
         <Box height="500px" style={{"borderStyle": "solid", "borderColor": "green"}} m={1}>
-            <Grid
-                container
-                direction="row"
-                justify="center"
-                alignItems="center"
-                style={{"borderStyle": "solid", "borderColor": "purple", "height": "300px"}}
-            >
-                <Grid item xs={12}>
-                    <Grid container justify="center" spacing={1}>
-                        <Grid item>
-                            <img src={quarternote} alt="quarter note"></img>
-                        </Grid>
-                        <Grid item>
-                            <img src={quarternote} alt="quarter note"></img>
-                        </Grid>
-                        <Grid item>
-                            <img src={quarternote} alt="quarter note"></img>
-                        </Grid>
-                        <Grid item>
-                            <img src={quarternote} alt="quarter note"></img>
-                        </Grid>
-                    </Grid>
-                </Grid>   
-            </Grid>
+            {notes.map((notesRow, idx) => {
+                return <SheetRow notesRow={notesRow} rowId={idx} key={idx} />
+            })}
         </Box>
     )
 }
