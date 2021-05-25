49498f969c255426d9e9aca0a55faf00b960a574
Mitchell <mitchell_shelton@y7mail.com>
2021-05-25 19:23:48 +1000

    initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Rhythm Expert</p>
+    </div>
+  );
+}
+
+export default App;
