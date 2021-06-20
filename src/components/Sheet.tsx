24abfbc9440680d9f4c14993680cb0805ba7e489
Mitchell <mitchell_shelton@y7mail.com>
2021-06-20 22:47:21 +1000

    use gsap to drag and drop notes, not finished

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react';
 import Box from '@material-ui/core/Box';
 import SheetRow from './SheetRow';
 import Vex from 'vexflow';
+// typical import
+import gsap from "gsap";
+import Draggable from "gsap/Draggable";
+
+gsap.registerPlugin(Draggable); 
 
 export type SheetProps = {
    notes: string[][][];
@@ -68,24 +73,87 @@ const Sheet = ({notes}: SheetProps) => {
                 new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "qr" }),
                 new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "q" }),
             ];
-    
-            // console.log("style", notesMeasure[0].attrs);
-            // console.log("style 2", notesMeasure[0].attrs.el)
 
             // Helper function to justify and draw a 4/4 voice
             Vex.Flow.Formatter.FormatAndDraw(context, staveMeasure, notesMeasure);
 
             // notesMeasure[0].attrs.el.opacity = 0;
             console.log("stave", staveMeasure);
+            console.log("style 0", notesMeasure[0]);
             console.log("style", notesMeasure[0].attrs);
             console.log("style 2", notesMeasure[0].attrs.el);
 
-            notesMeasure[0].attrs.el.style.opacity = 0;
-
-            // console.log("el", document.getElementById(notesMeasure[0].attrs.el));
+            notesMeasure.forEach((note) => {
+                
+                // note.attrs.el.draggable();
+            })
+            // notesMeasure[0].attrs.el.style.opacity = 0;
 
             prevStaveMeasure = staveMeasure;
         }
+
+        let droppables = document.getElementsByClassName("vf-stavenote");
+
+        let startPos = {
+            x: 0,
+            y: 0
+        }
+
+        Draggable.create(droppables, {
+            bounds: "svg",
+            lockAxis: true,
+            onDrag: function() {
+                
+            },
+            onDragEnd: function() {
+                console.log("drag end");
+                let i = droppables.length;
+                let hit = false;
+                while (--i > -1) {
+                    if (this.hitTest(droppables[i])) {
+                        hit = true;
+                        console.log("hit", droppables[i])
+
+                        let droppableRect = droppables[i].getBoundingClientRect();
+                        console.log("droppable rect", droppableRect);
+
+                        let nextX = startPos.x - droppableRect.left; 
+
+                        // get position of droppable[i]
+
+                        // move droppable[i] to this starting position
+
+                        // move this to the droppable[i] starting position
+                        // gsap.fromTo(droppables[i], {x: droppableRect.x, y: droppableRect.y}, {x: startPos.x, y: startPos.y, duration: 1});
+                        gsap.to(droppables[i], {x: nextX, duration: 1});
+
+                        gsap.to(this.target, {x: -nextX});
+
+                    }
+                }
+
+                if (!hit) {
+                    let targetRect = this.target.getBoundingClientRect();
+                    console.log("startPos.x, targetRect.x", startPos.x, targetRect.x)
+                    let moveBackX = startPos.x - targetRect.x;
+                    console.log("moveBackX", moveBackX);
+                    gsap.to(this.target, {x: -40, duration: 1});
+                }
+            },
+            // onDragStart: function() {
+            //     console.log("drag start");
+            // }
+            onPress: function() {
+                console.log("on press");
+                let rect = this.target.getBoundingClientRect();
+                console.log(rect);
+                // let [x, y] = this.target.getAttribute("data-svg-origin").split(" ");
+                startPos.x = rect.left;
+                startPos.y = rect.bottom;
+                // console.log(x, y);
+            }
+            
+        });
 
     });
 
